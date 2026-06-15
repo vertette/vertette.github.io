64e5425a2383bc2509f3c79c5f64f73f2b795d40
vertette <vertettegd@gmail.com>
2026-06-15 09:51:38 +0200

    Add: Error page

---
+++ app/error.vue
@@ -0,0 +1,11 @@
+<template>
+  <ClientOnly>
+    <NuxtLayout>
+      <Row>
+        <h2>404 - page not found</h2>
+        <hr />
+        <p>If you feel like you've found this page in error, please contact <a href="mailto:vertettegd@gmail.com">the web master</a>.</p>
+      </Row>
+    </NuxtLayout>
+  </ClientOnly>
+</template>
