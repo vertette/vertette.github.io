292b29d7a22ef7e9b51d3808a66bffc6bdd34d69
vertette <vertettegd@gmail.com>
2026-06-21 20:37:23 +0200

    Fix: Pagination current page button

--- app/components/Pagination.vue
+++ app/components/Pagination.vue
@@ -77,7 +77,7 @@ pageNoIs(props.currentPage)
     <button
       v-for="(n, index) in pageArray"
       :key="index"
-      :class="n === pageNo ? 'bg-primary text-cta -my-0.5' : 'border-r'"
+      :class="n === pageNo ? 'bg-primary text-cta pointer-events-none -my-0.5' : 'border-r'"
       class="group rounded-none px-3 sm:px-4"
       :disabled="n === '...'"
       @click="pageNoIs(n)"
